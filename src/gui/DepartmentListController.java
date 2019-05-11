c2fa31eca3cb94f610486cfd121fd72a575861f0
tchaves <tchaves@a5solutions.com>
2019-05-10 21:46:26 -0300

    Department Service

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentServices;
 
 public class DepartmentListController implements Initializable {
+	
+	private DepartmentServices service;
 
 	@FXML
 	private TableView<Department> tableViewDepartment;
@@ -23,11 +29,16 @@ public class DepartmentListController implements Initializable {
 	private TableColumn<Department, String> tableColumnName;
 	@FXML
 	private Button btNew;
+	
+	private ObservableList<Department> obsList;
 
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
 	}
-
+	//Fundamento do principio SOLID para evitar acoplamento forte(Injecao de dependencia)
+	public void setDepartmentService(DepartmentServices service) {
+		this.service = service;
+	}
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
@@ -41,4 +52,14 @@ public class DepartmentListController implements Initializable {
 		Stage stage = (Stage) Main.getMainsScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
+	
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+	}
+	
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentServices;
 
 public class MainViewController implements Initializable {
 	@FXML
@@ -33,7 +34,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 
 	@FXML
@@ -67,4 +68,33 @@ public class MainViewController implements Initializable {
 
 		}
 	}
+	
+	private synchronized void loadView2(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVbox = loader.load();
+
+			Scene mainScene = Main.getMainsScene();
+			// Olhar Hierarquia do XML para entender isso aqui
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			// Pega o primeiro filho do VBox
+			Node mainMenu = mainVbox.getChildren().get(0);
+			// Limpa todos os filhos do mainVbox
+			mainVbox.getChildren().clear();
+			//Adiciona um mainMenu a ao mainVbox
+			mainVbox.getChildren().add(mainMenu);
+			//Adiciona os filhos de newVbox
+			mainVbox.getChildren().addAll(newVbox.getChildren());
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentServices());
+			controller.updateTableView();
+
+		} catch (IOException e) {
+			Alerts.showAlert("IOEXCEPTION", "Error loader view", e.getMessage(), AlertType.ERROR);
+
+		}
+	}
+	
+	
 }
